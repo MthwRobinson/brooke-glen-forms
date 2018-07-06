d70b87ffcba3eb99c57943d70c830e5dea20214c
Matt Robinson <mthw.wm.robinson@gmail.com>
2018-07-05 22:09:29 -0400

    styled the header

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+
+import Header from './components/Header/Header';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         Hello world!
       </div>
     );
---
+++ ui/src/components/Header/Header.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Navbar } from 'react-bootstrap';
+
+import './Header.css';
+
+//<i className="fa fa-bars menu-button pull-right"></i>
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        <Navbar fluid>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#home">Brooke Glen Form Manager</a>
+            </Navbar.Brand>
+            <Navbar.Form className='pull-right menu-icon'>
+              <i className="fa fa-bars"></i>
+            </Navbar.Form>
+          </Navbar.Header>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default Header;
