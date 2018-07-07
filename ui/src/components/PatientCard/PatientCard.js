7ff0a12cd164b19a73076ea7f8ab71e955362777
Matt Robinson <mthw.wm.robinson@gmail.com>
2018-07-07 12:01:28 -0400

    finished styling patien card

--- ui/src/components/PatientCard/PatientCard.js
+++ ui/src/components/PatientCard/PatientCard.js
@@ -7,12 +7,22 @@ class PatientCard extends Component {
   render() {
     return (
       <div className="PatientCard" border>
-        <Col xs={2} sm={2} md={2} lg={2}>
+        <Col xs={4} sm={4} md={4} lg={3}>
           <img 
             className='patient-image'
             src='/images/placeholder-headshot.png'
             height='135'
           />
+        </Col>
+        <Col xs={8} sm={8} md={8} lg={9}>
+          <div className='patient-info'>
+            <b>Name:</b> John Doe<br/>
+            <b>Updated:</b> 07/07/18 8:18 PM<br/>
+            <b>Unit:</b> Long Term Care<br/>
+            <b>Obs. Level:</b> Q15<br/>
+            <b>Precautions:</b> Fall, Aggression, Seizure, Withdrawal, Respiratory
+
+          </div>
         </Col>
 
       </div>
