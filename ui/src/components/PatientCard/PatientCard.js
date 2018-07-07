11aef79ea475f94c944a0def0cf604dd4ed07ed3
Matt Robinson <mthw.wm.robinson@gmail.com>
2018-07-07 12:13:32 -0400

    made images disappear on small screens

--- ui/src/components/PatientCard/PatientCard.js
+++ ui/src/components/PatientCard/PatientCard.js
@@ -7,14 +7,14 @@ class PatientCard extends Component {
   render() {
     return (
       <div className="PatientCard" border>
-        <Col xs={4} sm={4} md={4} lg={3}>
+        <Col xs={12} sm={12} md={4} lg={3}>
           <img 
-            className='patient-image'
+            className='patient-image hidden-sm hidden-xs'
             src='/images/placeholder-headshot.png'
             height='135'
           />
         </Col>
-        <Col xs={8} sm={8} md={8} lg={9}>
+        <Col xs={12} sm={12} md={8} lg={9}>
           <div className='patient-info'>
             <b>Name:</b> John Doe<br/>
             <b>Updated:</b> 07/07/18 8:18 PM<br/>
