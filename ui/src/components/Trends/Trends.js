072e9dd2b58615d1b9d6b688a68654a5f2d5d67d
Matt Robinson <mthw.wm.robinson@gmail.com>
2018-07-08 20:14:18 -0400

    created example graph

--- ui/src/components/Trends/Trends.js
+++ ui/src/components/Trends/Trends.js
@@ -4,29 +4,56 @@ import Plot from 'react-plotly.js';
 
 import './Trends.css';
 
+const PATIENTS = require('./../../data/dummyPatients.json');
+
 class Trends extends Component {
+  aggregateCounts = () => {
+    var counts = {};
+    for(var i=0; i<PATIENTS.length; i++){
+      let patient = PATIENTS[i];
+      for(var j=0; j<patient.precautions.length; j++){
+        let precaution = patient.precautions[j];
+        if(precaution in counts){
+          counts[precaution] += 1;
+        } else {
+          counts[precaution] = 1;
+        }
+      }
+    }
+    return counts;
+  }
+
   render() {
+    const counts = this.aggregateCounts();
+    var x = [];
+    var y= [];
+    for(var key in counts){
+      x.push(key);
+      y.push(counts[key]);
+    }
+
     return (
       <div className="Trends">
         <Row>
           <h2>Trends</h2><hr/>
           <h4>Track trends in the patient population.</h4>
         </Row>
-        <Row>
+        <Row className='plot-container'>
           <Plot
           data={[
             {
               type: 'bar', 
               marker: {color: '#828f5c'},
-              x: [1, 2, 3], 
-              y: [2, 5, 3]
+              x: x, 
+              y: y
             },
           ]}
           layout={{
-              width: 1000, 
+              width: 900, 
               height: 600, 
               title: 'Precautions in the Patient Population',
-              showLegend: false
+            showLegend: false,
+            yaxis: { tickformat: 'd' }
             
           }}
           config={{staticPlot:true}}
