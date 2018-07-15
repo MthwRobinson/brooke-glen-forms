f2fc1b49cf8bc3fb8d3db0069925a0eb2cef8a3f
Matt Robinson <mthw.wm.robinson@gmail.com>
2018-07-15 18:42:11 -0400

    added filter to patient records

--- ui/src/components/PatientRecords/PatientRecords.js
+++ ui/src/components/PatientRecords/PatientRecords.js
@@ -17,19 +17,75 @@ class PatientRecords extends Component {
   constructor(props){
     super(props);
     this.state = {
-      unit: 'Unit 1',
+      unit: 'All',
+      obsLevel: 'All',
+      precaution: 'Any',
+      patients: PATIENTS
     }
 
-    this.handleChange = this.handleChange.bind(this);
+    this.handleUnit = this.handleUnit.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleObservation = this.handleObservation.bind(this);
+    this.handlePrecautions = this.handlePrecautions.bind(this);
   }
 
   handleSubmit(event) {
     console.log('submitted');
   }
 
-  handleChange(event) {
+  handleUnit(event) {
     this.setState({unit: event.target.value});
+  }
+  
+  handleObservation(event) {
+    this.setState({obsLevel: event.target.value});
+  }
+
+  handlePrecautions(event) {
+    this.setState({precaution: event.target.value});
+  }
+
+  clearFilter = () => {
+    this.setState({
+      unit: 'All',
+      obsLevel: 'All',
+      precaution: 'Any',
+      patients: PATIENTS
+    })
+  }
+
+  handleFilter = () => {
+    let patients = [];
+
+    for(var i=0; i<PATIENTS.length; i++){
+      let patient = PATIENTS[i];
+      let keepPatient = true;
+      // Filter on Unit
+      if(this.state.unit !== 'All'){
+        if(patient.unit !== this.state.unit){
+          keepPatient = false;
+        }
+      }
+
+      // Filter on Observation level
+      if(this.state.obsLevel !== 'All'){
+        if(patient.obsLevel !== this.state.obsLevel){
+          keepPatient = false;
+        }
+      }
+
+      //Filter on precautions
+      if(this.state.precaution !== 'Any'){
+        if(!patient.precautions.includes(this.state.precaution)){
+          keepPatient = false;
+        }
+      }
+
+      if(keepPatient){
+        patients.push(patient);
+      }
+    }
+    this.setState({patients: patients});
   }
 
   renderTable = () => {
@@ -46,7 +102,7 @@ class PatientRecords extends Component {
             </tr>
           </thead>
           <tbody>
-            {PATIENTS.map((patient, index) => {
+            {this.state.patients.map((patient, index) => {
               return(
               <tr key={index} onClick={()=>this.props.selectPatient(
                 patient.name,
@@ -78,20 +134,28 @@ class PatientRecords extends Component {
           <Col xs={3} sm={3} md={3} lg={3}>
             <FormGroup controlId="formControlsSelect">
               <ControlLabel>Unit</ControlLabel>
-              <FormControl componentClass="select" placeholder="all">
-                <option value="all">All</option>
-                <option value="longTerm">Long Term Care</option>
-                <option value="shortTerm">Short Term Care</option>
+              <FormControl 
+                componentClass="select"
+                value={this.state.unit}
+                onChange={this.handleUnit}
+              >
+                <option value="All">All</option>
+                <option value="Long Term Care">Long Term Care</option>
+                <option value="Short Term Care">Short Term Care</option>
               </FormControl>
           </FormGroup>
           </Col>
           <Col xs={3} sm={3} md={3} lg={3}>
             <FormGroup controlId="formControlsSelect">
               <ControlLabel>Observation Level</ControlLabel>
-              <FormControl componentClass="select" placeholder="Q15">
-                <option value="all">All</option>
+              <FormControl 
+                componentClass="select"
+                value={this.state.obsLevel}
+                onChange={this.handleObservation}
+              >
+                <option value="All">All</option>
                 <option value="Q15">Q15</option>
-                <option value="1on1">1:1</option>
+                <option value="1:1">1:1</option>
                 <option value="CPAP">CPAP</option>
               </FormControl>
             </FormGroup>
@@ -99,16 +163,31 @@ class PatientRecords extends Component {
           <Col xs={3} sm={3} md={3} lg={3}>
             <FormGroup controlId="formControlsSelect">
               <ControlLabel>Precautions</ControlLabel>
-              <FormControl componentClass="select" placeholder="any">
-                <option value="any">Any</option>
-                <option value="Q15">Aggression</option>
-                <option value="1on1">Fall</option>
-                <option value="CPAP">Respiratory</option>
+              <FormControl 
+                componentClass="select"
+                value={this.state.precaution}
+                onChange={this.handlePrecautions}
+              >
+                <option value="Any">Any</option>
+                <option value="Aggression">Aggression</option>
+                <option value="Fall">Fall</option>
+                <option value="Respiratory">Respiratory</option>
               </FormControl>
             </FormGroup>
           </Col>
-          <Col xs={2} sm={2} md={2} lg={2}>
-            <Button className='submit-button' type="submit">Filter</Button>
+          <Col xs={1} sm={1} md={1} lg={1}>
+            <Button 
+              className='submit-button' 
+              type="submit"
+              onClick={()=>this.handleFilter()}
+            >Filter</Button>
+          </Col>
+          <Col xs={1} sm={1} md={1} lg={1}>
+            <Button 
+              className='submit-button' 
+              type="submit"
+              onClick={()=>this.clearFilter()}
+            >Clear</Button>
           </Col>
         </form>
       </div>
