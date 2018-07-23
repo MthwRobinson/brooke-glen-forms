901c2d391bc9111d66edfca58186fc83864c667d
Matt Robinson <mthw.wm.robinson@gmail.com>
2018-07-23 18:47:44 -0400

    added postgres connection

---
+++ services/bg_forms/configuration.py
@@ -0,0 +1,11 @@
+configs = {
+    'DEV': {
+        PG_DATABASE: 'bg_forms_dev',
+    },
+    'TEST': {
+        PG_DATABASE: 'bg_forms_dev',
+    },
+    'PROD': {
+        PG_DATABASE: 'bg_forms_dev',
+    }
+}
--- services/bg_forms/dbops/dbops.py
+++ services/bg_forms/dbops/dbops.py
@@ -7,8 +7,27 @@ import daiquiri
 import pandas as pd
 import psycopg2
 
+import bg_forms.configuration as conf
+
 class DBOps(object):
     """ Manage database operations """
-    def __init__(self):
-        self.pg_user = os.getenv('POSTGRES_USER')
-        self.pg_pass = os.getenv('POSTGRES_PASSWORD')
+    def __init__(self, environment='DEV'):
+        daiquiri.setup(level=LOGGING.INFO)
+        self.logger = daiquiri.getLogger(__name__)
+        self.conn = self.connect()
+        
+    def connect():
+        """ Connects to the postgres database """
+        pg_user = os.getenv('POSTGRES_USER')
+        pg_pass = os.getenv('POSTGRES_PASSWORD')
+        pg_host= os.getenv('POSTGRES_HOST')
+        pg_port = int(os.getenv('POSTGRES_PORT'))
+        pg_db = conf.configs[environment]['PG_DATABASE']
+        connection = psycopg2.connect(
+            user=pg_user,
+            password=pg_pass,
+            host=pg_host,
+            port=pg_port,
+            dbname=pg_db
+        )
+        return connection
