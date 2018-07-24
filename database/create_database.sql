78a05fd405160d0e1e3692939996c75187adcc0d
Matt Robinson <mthw.wm.robinson@gmail.com>
2018-07-23 23:35:44 -0400

    added created schema commands

--- database/create_database.sql
+++ database/create_database.sql
@@ -1,13 +1,13 @@
 CREATE DATABASE bg_forms_prod;
 \connect bg_forms_prod;
-CREATE SCHEMA bg_forms;
+CREATE SCHEMA IF NOT EXISTS bg_forms;
 
 CREATE DATABASE bg_forms_dev;
 \connect bg_forms_dev;
-CREATE SCHEMA bg_forms;
+CREATE SCHEMA IF NOT EXISTS bg_forms;
 
 CREATE DATABASE bg_forms_test;
 \connect db_forms_test;
-CREATE SCHEMA bg_forms;
+CREATE SCHEMA IF NOT EXISTS bg_forms;
 
 
